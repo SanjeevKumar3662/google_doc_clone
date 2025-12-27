f9908b416382acec19f9783f5eb5898501a05af9
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-12-27 11:56:08 +0530

    Implement Quill text editor with custom toolbar and add styles

--- src/component/TextEditor.jsx
+++ src/component/TextEditor.jsx
@@ -1,12 +1,35 @@
 import "quill/dist/quill.snow.css";
 import Quill from "quill";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import "../styles.css";
 
 const TextEditor = () => {
+  const wrapperRef = useRef();
+
+  const TOOLBAR_OPTIONS = [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ font: [] }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["bold", "italic", "underline"],
+    [{ color: [] }, { background: [] }],
+    [{ script: "sub" }, { script: "super" }],
+    [{ align: [] }],
+    ["image", "blockquote", "code-block"],
+    ["clean"],
+  ];
+
   useEffect(() => {
-    new Quill("#container", { theme: "snow" });
+    wrapperRef.innerHTML = "";
+
+    const editor = document.createElement("div");
+    wrapperRef.current.append(editor);
+    new Quill(editor, { theme: "snow", modules: { toolbar: TOOLBAR_OPTIONS } });
+
+    return () => {
+      wrapperRef.innerHTML = "";
+    };
   }, []);
-  return <div id="container"></div>;
+  return <div className="container" ref={wrapperRef}></div>;
 };
 
 export default TextEditor;
