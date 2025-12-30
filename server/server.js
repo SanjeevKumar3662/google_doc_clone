d3f0892eab6b6b46ce9243e4df6aef60e8eff427
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-12-30 17:32:35 +0530

    feat: implement document loading and saving functionality in socket.io connection

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import mongoose from "mongoose";
+import { Document } from "./models/document.model.js";
 
+await mongoose.connect(process.env.DB_URI);
+// console.log(process.env.DB_URI);
 const app = express();
 const server = createServer(app);
 
@@ -12,16 +19,23 @@ const io = new Server(server, {
   },
 });
 
+const defaultValue = "";
+
 io.on("connection", (socket) => {
   // console.log("connected");
-  socket.on("get-document", (documentId) => {
-    const data = "hi, its me mario";
+  socket.on("get-document", async (documentId) => {
+    const document = await findOrCreateDocument(documentId);
+    // const data = "hi, its me mario";
     // console.log(documentId);
     socket.join(documentId);
-    socket.emit("load-document", data);
+    socket.emit("load-document", document.data);
 
     socket.on("send-changes", (delta) => {
       socket.broadcast.to(documentId).emit("receive-changes", delta);
+    });
+
+    socket.on("save-document", async (data) => {
+      await Document.findByIdAndUpdate(documentId, { data });
     });
   });
 });
@@ -29,3 +43,11 @@ io.on("connection", (socket) => {
 server.listen(3000, () => {
   console.log("Server is running");
 });
+
+async function findOrCreateDocument(id) {
+  if (id == null) return;
+
+  const document = await Document.findById(id);
+  if (document) return document;
+  return await Document.create({ _id: id, data: defaultValue });
+}
